6d971a2c3f6c915aa1aea0918a878b5d41935bcd
tee <pulserateX@proton.me>
2024-01-31 04:00:26 +0100

    gas optimization

--- contracts/gasTrackerExample.sol
+++ contracts/gasTrackerExample.sol
@@ -2,15 +2,14 @@
 pragma solidity ^0.8.22;
 
 contract GasTracker {
+    struct UserData {
+        mapping(uint256 => uint256) dailyGas;
+    }
 
     mapping(address => UserData) private userData;
     mapping(uint256 => uint256) public totalGasPerDayUsed;
     uint256 public contractDeployedTime;
 
-    struct UserData {
-        mapping(uint256 => uint256) dailyGas;
-        uint256 startGas;
-    }
     constructor() {
         contractDeployedTime = block.timestamp;
     }
@@ -19,23 +18,14 @@ contract GasTracker {
         return (block.timestamp - contractDeployedTime) / 1 days;
     }
 
-    function trackGasUsageStart() internal {
-        address caller = msg.sender;
-        userData[caller].startGas = gasleft();
-    }
-
-    function trackGasUsageEnd() internal {
-        address caller = msg.sender;
-        uint256 currentDay = getCurrentDay();
-        uint256 gasSpentLocal = userData[caller].startGas - gasleft();
-        userData[caller].dailyGas[currentDay] += gasSpentLocal;
-        totalGasPerDayUsed[currentDay] += gasSpentLocal;
-    }
-
     function exampleFunction() public {
-        trackGasUsageStart();
-        // logic here
-        trackGasUsageEnd();
-    }
+        uint256 startGas = gasleft();
+        
+        // Logic here
 
+        uint256 gasSpent = startGas - gasleft();
+        uint256 currentDay = getCurrentDay();
+        userData[msg.sender].dailyGas[currentDay] += gasSpent;
+        totalGasPerDayUsed[currentDay] += gasSpent;
+    }
 }
