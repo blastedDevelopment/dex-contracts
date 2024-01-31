67841af57cc93c969c2a2c068d1c0d5089e5f847
tee <pulserateX@proton.me>
2024-01-31 04:16:59 +0100

    Update gasTrackerExample.sol

--- contracts/gasTrackerExample.sol
+++ contracts/gasTrackerExample.sol
@@ -14,18 +14,26 @@ contract GasTracker {
         contractDeployedTime = block.timestamp;
     }
 
-    function getCurrentDay() public view returns (uint256) {
-        return (block.timestamp - contractDeployedTime) / 1 days;
-    }
-
-    function exampleFunction() public {
-        uint256 startGas = gasleft();
-        
-        // Logic here
-
+    function updateGasUsage(uint256 startGas) internal {
         uint256 gasSpent = startGas - gasleft();
         uint256 currentDay = getCurrentDay();
         userData[msg.sender].dailyGas[currentDay] += gasSpent;
         totalGasPerDayUsed[currentDay] += gasSpent;
+    }
+
+    function exampleFunction() public {
+        uint256 startGas = gasleft();
+
+        // Logic here
+
+        updateGasUsage(startGas);
+    }
+
+    function getCurrentDay() public view returns (uint256) {
+        return (block.timestamp - contractDeployedTime) / 1 days;
+    }
+
+    function viewUserGasUsage(address user, uint256 day) public view returns (uint256) {
+        return userData[user].dailyGas[day];
     }
 }
